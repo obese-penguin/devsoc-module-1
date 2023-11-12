2d0fa5d54938338f75715a7f35be3aa3bb7df493
obese-penguin <sketchyakounts@gmail.com>
2023-11-13 00:10:04 +0530

    clr screen after calculation feature

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
+var calc_done = false;
+
 function display(val) {
+
 	const input_line = document.getElementsByClassName('input-line')[0]; 
+	const output_line = document.getElementsByClassName('output-line')[0];
+	
+	if (calc_done) {
+		input_line.innerText = "";
+		calc_done = false;
+	}
 	input_line.innerHTML += val;
 }
 
-function clear() {
+function clr() {
 	const input_line = document.getElementsByClassName('input-line')[0]; 
 	const output_line = document.getElementsByClassName('output-line')[0];
-	input_line.value = "";
-	output_line.value = "";
+	input_line.innerText = "";
+	output_line.innerText = "";
 }
 
 function answer() {
 	const input_line = document.getElementsByClassName('input-line')[0]; 
 	const output_line = document.getElementsByClassName('output-line')[0];
+	if (calc_done) {
+		input_line.innerText = "";
+		output_line.innerText = "";
+		calc_done = false;
+		return;
+	}
+
 	var output = input_line.innerText
 	output_line.innerText = eval(output);	
+	calc_done = true;
 }
 
 function del() {
